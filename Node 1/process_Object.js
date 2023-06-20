4c865955af9d54cec2770c6e28dd539c10686f5b
shaanMS <96082415+shaanMS@users.noreply.github.com>
2023-06-21 01:28:46 +0530

    Node

--- Node 1/process_Object.js
+++ Node 1/process_Object.js
@@ -30,4 +30,40 @@ console.log(process.argv[1])
 
 console.log('\n\n\n')
 
-console.log(process.argv[2])
+//console.log(process.argv[2])
+
+const v1 = process.argv[2];
+
+let msg = ' hrrrrrrrrrrr...............${v1}';
+
+console.log(msg);
+
+console.log("\n\n\n\n")
+
+console.warn("Hello");
+
+var s = "shaan";
+
+console.log(s)
+
+console.count()
+
+console.count()
+
+console.clear()
+
+for(let x = 0;x<1000;x++)
+{
+
+console.table([
+    { a: x, b: 2-x/6+x/568 }, 
+    { a: 3000000%x, b: 7-x/55555, c: 'y' }
+]);
+
+if (x > 500)
+     var t = console.warn("Alert !!!!!!!")
+ 
+ console.log(t);
+ 
+ console.info()
+}
